12c2b596e4af30427a77e7fd57aca0649d8cb10d
Alexandre <alexandre.chevallier4@gmail.com>
2016-01-24 12:21:36 +0100

    more big

--- view/liste_generale.php
+++ view/liste_generale.php
@@ -7,32 +7,33 @@
         $user = $result->fetch();
         echo 'Bonjour '.$user['prenom'].' '.$user['nom'].' votre classe est '.$user['classe'].'</br></br> Voici votre liste de fourniture :</br>';
     ?>
-    <div class="drag">
-        <ul>
-            <?php
-                $result = $bdd->query("SELECT f.nom as nom, MAX(l.quantite) as quantite, m.nom as matiere
-                                FROM fourniture f, liste l, link_eleve le, link_prof lp, matiere m
-                                WHERE f.id = l.fourniture_id
-                                AND lp.classe_id = le.classe_id
-                                AND l.prof_id = lp.prof_id
-                                AND f.matiere_id = m.id
-                                AND le.eleve_id = ".$_SESSION['id']."
-                                GROUP BY m.id,f.id"
-                            );
-                $i = 1;
-                foreach ($result as $row) {            
-                  echo '<li draggable="true"  ondragstart="drag(event)" data-value="'.$i.'" id="'.$i.'">'.$row['quantite'].' - '.$row['nom'].' - '.$row['matiere'].'</li>';
-                  $i++;
-                }
-            ?>
-        </ul>
-    </div>
-    <br/>
-    <br/>
     <div id="div_classe_col" class="div_">
         <div class="Row">
             <div class="Column">
-            Je n'ai pas
+                Liste
+                <div class="drag">
+                    <ul>
+                        <?php
+                            $result = $bdd->query("SELECT f.nom as nom, MAX(l.quantite) as quantite, m.nom as matiere
+                                            FROM fourniture f, liste l, link_eleve le, link_prof lp, matiere m
+                                            WHERE f.id = l.fourniture_id
+                                            AND lp.classe_id = le.classe_id
+                                            AND l.prof_id = lp.prof_id
+                                            AND f.matiere_id = m.id
+                                            AND le.eleve_id = ".$_SESSION['id']."
+                                            GROUP BY m.id,f.id"
+                                        );
+                            $i = 1;
+                            foreach ($result as $row) {            
+                              echo '<li draggable="true"  ondragstart="drag(event)" data-value="'.$i.'" id="'.$i.'">'.$row['quantite'].' - '.$row['nom'].' - '.$row['matiere'].'</li>';
+                              $i++;
+                            }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="Column">
+                Je n'ai pas
                 <div class="drag">
                     <ul ondrop="drop(event)" ondragover="allowDrop(event)">
                     </ul>
